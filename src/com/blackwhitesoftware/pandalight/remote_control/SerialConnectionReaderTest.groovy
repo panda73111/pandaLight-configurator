21d96b10059514bea6a9cbe030b4196ad257a337
Sebastian Hüther <sebastian.huether@gmx.de>
2015-10-13 18:52:00 +0200

    Tried out Groovy unit testing... I still don't like Java...

---
+++ src/com/blackwhitesoftware/pandalight/remote_control/SerialConnectionReaderTest.groovy
@@ -0,0 +1,11 @@
+package com.blackwhitesoftware.pandalight.remote_control
+
+/**
+ * Created by hudini on 11.10.2015.
+ */
+class SerialConnectionReaderTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+}
