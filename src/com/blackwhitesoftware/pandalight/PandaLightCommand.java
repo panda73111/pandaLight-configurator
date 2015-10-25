6616185b5664a97a2afd5c66d291449bec5ba494
Sebastian Hüther <sebastian.huether@gmx.de>
2015-10-25 20:59:26 +0100

    Added PandaLightCommand

---
+++ src/com/blackwhitesoftware/pandalight/PandaLightCommand.java
@@ -0,0 +1,21 @@
+package com.blackwhitesoftware.pandalight;
+
+/**
+ * Created by hudini on 25.10.2015.
+ */
+public enum PandaLightCommand {
+    SYSINFO((byte) 0x00),
+    REBOOT((byte) 0x01),
+    LOAD_SETTINGS_FROM_FLASH((byte) 0x20),
+    WRITE_SETTINGS_TO_FLASH((byte) 0x21),
+    LOAD_SETTINGS_FROM_UART((byte) 0x22),
+    WRITE_SETTINGS_TO_UART((byte) 0x23),
+    LOAD_BITFILE_FROM_UART((byte) 0x40),
+    WRITE_BITFILE_TO_UART((byte) 0x41);
+
+    private final byte byteCommand;
+
+    PandaLightCommand(byte byteCommand) {
+        this.byteCommand = byteCommand;
+    }
+}
