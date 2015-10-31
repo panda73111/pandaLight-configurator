d9962796f865b0a7da4a1770ce92fc0d7ab50df8
Sebastian Hüther <sebastian.huether@gmx.de>
2015-10-31 15:58:50 +0100

    Clearing list of expected packets on connect

--- src/com/blackwhitesoftware/pandalight/remote_control/PandaLightProtocol.java
+++ src/com/blackwhitesoftware/pandalight/remote_control/PandaLightProtocol.java
@@ -25,6 +25,7 @@ public class PandaLightProtocol {
             @Override
             public void connected() {
                 inputDataBuffer.clear();
+                expectedPackets.clear();
             }
 
             @Override
