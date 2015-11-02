31dc7691928f305e07c55a22f8a3a619013788d6
Sebastian Hüther <sebastian.huether@gmx.de>
2015-11-02 15:03:47 +0100

    Added constructor and data field to PandaLightSettingsPacket

--- src/com/blackwhitesoftware/pandalight/remote_control/PandaLightSettingsPacket.java
+++ src/com/blackwhitesoftware/pandalight/remote_control/PandaLightSettingsPacket.java
@@ -4,4 +4,13 @@ package com.blackwhitesoftware.pandalight.remote_control;
  * Created by sebastian on 30.10.15.
  */
 public class PandaLightSettingsPacket extends PandaLightPacket {
+    private byte[] data;
+
+    public PandaLightSettingsPacket(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
 }
