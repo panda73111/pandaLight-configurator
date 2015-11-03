20a328d9bc15f63f8ae09dcb5612e35e281756d4
Sebastian Hüther <sebastian.huether@gmx.de>
2015-11-03 15:34:15 +0100

    Added getter methods in PandaLightSysinfoPacket

--- src/com/blackwhitesoftware/pandalight/remote_control/PandaLightSysinfoPacket.java
+++ src/com/blackwhitesoftware/pandalight/remote_control/PandaLightSysinfoPacket.java
@@ -1,16 +1,36 @@
 package com.blackwhitesoftware.pandalight.remote_control;
 
+import java.util.Arrays;
+
 /**
  * Created by sebastian on 30.10.15.
  */
 public class PandaLightSysinfoPacket extends PandaLightPacket {
     private byte[] data;
+    private byte[] magic;
+    private int majorVersion;
+    private int minorVersion;
 
     public PandaLightSysinfoPacket(byte[] data) {
         this.data = data;
+        magic = Arrays.copyOfRange(data, 0, 9);
+        majorVersion = data[10];
+        minorVersion = data[11];
     }
 
     public byte[] getData() {
         return data;
+    }
+
+    public byte[] getMagic() {
+        return magic;
+    }
+
+    public int getMajorVersion() {
+        return majorVersion;
+    }
+
+    public int getMinorVersion() {
+        return minorVersion;
     }
 }
