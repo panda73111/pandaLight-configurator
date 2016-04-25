3da0045fda4aa108bba0e348300d32f923c2498d
Sebastian Hüther <sebastian.huether@gmx.de>
2016-04-25 22:47:53 +0200

    Testing writing 2KB of bitfile

--- src/com/blackwhitesoftware/pandalight/test/SerialTest.java
+++ src/com/blackwhitesoftware/pandalight/test/SerialTest.java
@@ -9,9 +9,10 @@ import jssc.SerialPortException;
  * Created by hudini on 05.11.2015.
  */
 public class SerialTest {
+    private static boolean paused = false;
+
     public static void main(String[] pArgs) throws SerialPortException {
         try
-
         {
             final SerialPort serialPort = new SerialPort("COM24");
             serialPort.openPort();
@@ -24,10 +25,16 @@ public class SerialTest {
             serialPort.setFlowControlMode(
                     SerialPort.FLOWCONTROL_RTSCTS_IN | SerialPort.FLOWCONTROL_RTSCTS_OUT);
 
-            serialPort.setEventsMask(SerialPortEvent.RXCHAR);
+            serialPort.setEventsMask(SerialPortEvent.RXCHAR | SerialPortEvent.CTS);
             serialPort.addEventListener(new SerialPortEventListener() {
                 @Override
                 public void serialEvent(SerialPortEvent serialPortEvent) {
+                    if (serialPortEvent.isCTS()) {
+                        System.out.println("CTS event: " + serialPortEvent.getEventValue());
+                        paused = serialPortEvent.getEventValue() == 0;
+                        return;
+                    }
+
                     try {
                         System.out.println("new bytes: " + serialPort.readHexString());
                     } catch (SerialPortException e) {
@@ -42,6 +49,34 @@ public class SerialTest {
             Thread.sleep(500);
             System.out.println("sending acknowledge for packet 0x00");
             serialPort.writeBytes(new byte[]{0x66, 0x00, 0x66});
+            Thread.sleep(2000);
+            System.out.println("sending 2KB bitfile");
+            serialPort.writeBytes(new byte[]{0x65, 0x01, 0x00, 0x40, (byte) 0xA6});
+            serialPort.writeBytes(new byte[]{0x00}); // bitfile index
+            serialPort.writeBytes(new byte[]{0x08, 0x00, 0x00}); // bitfile size
+            for (byte packetNumber = 2; packetNumber < 2 + 8; packetNumber++) {
+                if (paused) {
+                    System.out.println("paused sending");
+                    while (paused)
+                        Thread.sleep(100);
+                    System.out.println("resumed sending");
+                }
+
+                System.out.println("sending packet " + packetNumber);
+                int checksum = 0x65 + packetNumber - 2 + 0xFF;
+                serialPort.writeBytes(new byte[]{0x65, (byte) (packetNumber - 2), (byte) 0xFF});
+
+                for (int i = 0; i < 256; i++) {
+                    byte data = (byte) ((packetNumber & 0x07) << 5);
+                    data |= i & 0x1F;
+
+                    // 3 bit packet number + 5 bit counter
+                    serialPort.writeByte(data);
+                    checksum += data;
+                }
+
+                serialPort.writeByte((byte) (checksum & 0xFF));
+            }
             Thread.sleep(2000);
 
             serialPort.closePort();
