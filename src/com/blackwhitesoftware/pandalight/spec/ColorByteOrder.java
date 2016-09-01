ec648b49043c4ed12b5fed6b9e7cfe0edab86072
Sebastian Hüther <sebastian.huether@gmx.de>
2016-09-01 13:18:08 +0200

    Added indeces used for PandaLightSettings to ColorByteOrder

--- src/com/blackwhitesoftware/pandalight/spec/ColorByteOrder.java
+++ src/com/blackwhitesoftware/pandalight/spec/ColorByteOrder.java
@@ -1,5 +1,23 @@
 package com.blackwhitesoftware.pandalight.spec;
 
 public enum ColorByteOrder {
-    RGB, RBG, BRG, BGR, GRB, GBR
+    RGB(0),
+    RBG(1),
+    GRB(2),
+    GBR(3),
+    BRG(4),
+    BGR(5);
+
+    /**
+     * The number representation of this configuration
+     */
+    private final int mIndex;
+
+    ColorByteOrder(int index) {
+        mIndex = index;
+    }
+
+    public int getIndex() {
+        return mIndex;
+    }
 }
