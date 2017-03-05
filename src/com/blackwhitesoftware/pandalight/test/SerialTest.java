f1cf320e151504d155f37ea77b3a460153aa3636
Sebastian Hüther <sebastian.huether@gmx.de>
2017-03-05 02:03:55 +0100

    Removed transport layer from SerialTest

--- src/com/blackwhitesoftware/pandalight/test/SerialTest.java
+++ src/com/blackwhitesoftware/pandalight/test/SerialTest.java
@@ -14,7 +14,7 @@ public class SerialTest {
     public static void main(String[] pArgs) throws SerialPortException {
         try
         {
-            final SerialPort serialPort = new SerialPort("COM24");
+            final SerialPort serialPort = new SerialPort("COM7");
             serialPort.openPort();
             serialPort.setParams(
                     SerialPort.BAUDRATE_115200,
@@ -45,44 +45,18 @@ public class SerialTest {
 
             Thread.sleep(2000);
             System.out.println("sending SYSINFO command");
-            serialPort.writeBytes(new byte[]{0x65, 0x00, 0x00, 0x00, 0x65});
-            Thread.sleep(500);
-            System.out.println("sending acknowledge for packet 0x00");
-            serialPort.writeBytes(new byte[]{0x66, 0x00, 0x66});
+            serialPort.writeBytes(new byte[]{0x00});
             Thread.sleep(2000);
             System.out.println("sending 400KB bitfile");
             serialPort.writeBytes(new byte[]{
-                    0x65, 0x01,
-                    0x04,             // payload length
                     0x40,             // command
                     0x00,             // bitfile index
                     0x06, 0x40, 0x00, // bitfile size
-                    (byte) 0xF0       // checksum
             });
 
-            for (int packetNumber = 2; packetNumber < 2 + 1600; packetNumber++) {
-                if (paused) {
-                    System.out.println("paused sending");
-                    while (paused)
-                        Thread.sleep(100);
-                    System.out.println("resumed sending");
-                }
-
-                System.out.println("sending packet " + packetNumber);
-                int checksum = 0x65 + packetNumber + 0xFF;
-                serialPort.writeBytes(new byte[]{0x65, (byte) (packetNumber & 0xFF), (byte) 0xFF});
-
-                for (int i = 0; i < 256; i++) {
-                    byte data = (byte) (((packetNumber - 2) & 0x07) << 5);
-                    data |= i & 0x1F;
-
-                    // 3 bit packet number + 5 bit counter
-                    serialPort.writeByte(data);
-                    checksum += data;
-                }
-
-                serialPort.writeByte((byte) (checksum & 0xFF));
-                Thread.sleep(100);
+            for (int i = 0; i < 400 * 1024; i++) {
+                byte data = (byte)(i & 0xFF);
+                serialPort.writeByte(data);
             }
             Thread.sleep(2000);
 
